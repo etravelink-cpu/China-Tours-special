683a2781598bcbf01326bb4e04cd58e8741f4604
Lyn <lynda@etravelink.com.au>
2026-07-26 06:35:43 +1000

    PV+Funtrip 暂不可见(Online_Visible=0)，仅中国美+Nova可见

--- assets/js/region-plans.js
+++ assets/js/region-plans.js
@@ -1,3 +1,3 @@
 window.REGION_PLANS = window.REGION_PLANS || {};
-window.REGION_VISIBLE = ['china', 'special', 'australia', 'nz', 'asia', 'other', 'america', 'europe', 'island'];
+window.REGION_VISIBLE = ['china', 'special', 'australia', 'nz'];
 window.FEATURED = [];
