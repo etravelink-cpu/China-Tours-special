86eba5cca6cd2b5f5ab7ffd9f340b9206765c1e4
etravelink-cpu <local@etrips>
2026-08-03 23:59:12 +1000

    列表页内嵌详情移除冗余的'查看完整详情页'按钮(内嵌预览已含完整内容, 删每产品重复入口; 独立detail.html不受影响)

--- assets/js/list.js
+++ assets/js/list.js
@@ -325,8 +325,7 @@
       "<div class='rp-tab-panel' data-tab='itinerary'>" + itinHtml + "</div>" +
       "<div class='rp-tab-panel' data-tab='cost'>" + costHtml + "</div>" +
       "<div class='rp-tab-panel' data-tab='notes'>" + noticeHtml + "</div>" +
-      "<div class='rp-tab-panel' data-tab='brochure'>" + broHtml + "</div>" +
-      "<div style='padding:18px 24px'><a href='detail.html?id=" + encodeURIComponent(t.id) + "' class='btn btn-primary' style='width:100%;text-align:center'>查看完整详情页</a></div>";
+      "<div class='rp-tab-panel' data-tab='brochure'>" + broHtml + "</div>";
 
     box.querySelectorAll(".rp-tab").forEach((tab) => {
       tab.addEventListener("click", () => {
